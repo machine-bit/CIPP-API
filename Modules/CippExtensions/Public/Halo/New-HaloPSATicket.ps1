a9565b11d93ca28d00be8959c11ecb4670917488
jamesrfusion <jamesr@fitm.co.uk>
2025-01-13 08:24:44 +0000

    Update New-HaloPSATicket.ps1
    Removed - Category Custom Field

--- Modules/CippExtensions/Public/Halo/New-HaloPSATicket.ps1
+++ Modules/CippExtensions/Public/Halo/New-HaloPSATicket.ps1
@@ -26,13 +26,7 @@ function New-HaloPSATicket {
         details_html               = $description
         donotapplytemplateintheapi = $true
         attachments                = @()
-# edited to pass the category       
-        customfields               = @{
-            id            = 416
-            value = 7
-        }
     }
-
 
     if ($Configuration.TicketType) {
         $object | Add-Member -MemberType NoteProperty -Name 'tickettype_id' -Value $Configuration.TicketType -Force
